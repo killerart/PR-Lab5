bb72e68d03df0da46192192d6b8696b79a558256
killerart <442234@mail.ru>
2021-04-04 19:40:08 +0300

    Add project files.

---
+++ Lab5.Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var tasks = Enumerable.Range(0, 10000).Select(x => Task.Run(() =>
+                {
+                    string host = "127.0.0.1";
+                    int port = 9876;
+                    using var client = new TcpClient(host, port);
+                    Console.WriteLine($"Client {x} connected to TCP server on {host}:{port}");
+
+                    using var stream = client.GetStream();
+                    using var writer = new StreamWriter(stream) { AutoFlush = true };
+                    writer.WriteLine(x);
+                    Console.WriteLine($"Sent message to server: {x}");
+
+                    using var reader = new StreamReader(stream);
+                    string message = reader.ReadLine();
+                    Console.WriteLine($"Received message from server: {message}");
+
+                    client.Close();
+
+                    Console.WriteLine($"Connection {x} closed");
+                })
+            );
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ Lab5.Server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Server
+{
+    class Program
+    {
+        static readonly TcpListener listener = new(IPAddress.Any, 9876);
+
+        static void Main()
+        {
+            listener.Start();
+            Console.WriteLine("Server listening for TCP on port 9876\n");
+
+            listener.BeginAcceptTcpClient(HandleConnection, listener);
+
+            Console.ReadKey(true);
+        }
+
+        static void HandleConnection(IAsyncResult result)
+        {
+            listener.BeginAcceptTcpClient(HandleConnection, listener);
+
+            var client = listener.EndAcceptTcpClient(result);
+            Console.WriteLine("Client connected");
+            using var stream = client.GetStream();
+
+            using var reader = new StreamReader(stream);
+            var message = reader.ReadLine();
+            Console.WriteLine($"Message received from client: {message}");
+
+            using var writer = new StreamWriter(stream) { AutoFlush = true };
+            writer.WriteLine(message);
+            Console.WriteLine($"Message sent to client: {message}");
+
+            client.Close();
+            Console.WriteLine($"Client {message} disconnected\n");
+
+            client.Dispose();
+        }
+    }
+}
